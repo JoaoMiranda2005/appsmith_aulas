20a6618f1b7b48bd910ed82370771e3818139121
JoaoMiranda2005 <joaomiranda2005>
2023-11-03 21:51:48 +0000

    nnnnn

---
+++ pages/Page1/jsobjects/JSObject1/JSObject1.js
@@ -0,0 +1,13 @@
+export default {
+	myVar1: [],
+	myVar2: {},
+	myFun1 () {
+		if (username.text === null) {
+			showAlert("Please enter a username");
+		}
+	},
+	async myFun2 () {
+		//	use async-await or promises
+		//	await storeValue('varName', 'hello world')
+	}
+}
